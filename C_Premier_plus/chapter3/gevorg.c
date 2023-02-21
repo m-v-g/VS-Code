602beaa5b425d9567621c5fb55753ad8382cfa1c
m-v-g <m-v-g@mail.ru>
2023-02-21 21:53:33 +0400

    111

--- C_Premier_plus/chapter3/gevorg.c
+++ C_Premier_plus/chapter3/gevorg.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(void)
 {
-    int a = 2376;
-    int b = 108;
+    int a = 120;
+    int b = 24;
     printf("ամբողջ = %d \n", a/b);
     printf("մնացորդ = %d \n", a%b);
     return 0;
